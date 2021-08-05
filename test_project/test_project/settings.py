4f545de312833026de0a75f763a1e02ce84358ee
Bars <barsbekclash@gmail.com>
2021-08-06 02:43:34 +0600

    Установка нужных зависимостей и подключение PostgreSql

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api_v1',
+    'rest_framework'
+
 ]
 
 MIDDLEWARE = [
@@ -74,9 +77,14 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test_project_db',
+        'USER': 'test_work_user',
+        'PASSWORD': 'test',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
